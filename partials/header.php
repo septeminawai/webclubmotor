ac13d62e527681cf718fb83b8710e7559638576c
sethy <89424441+septeminawai@users.noreply.github.com>
2024-09-06 10:33:19 +0900

    tambah logo

--- partials/header.php
+++ partials/header.php
@@ -3,10 +3,23 @@
         <div class="container">
             <div class="row py-2 mt-2 bg-light border shadow-md">
                 <div class="col-lg-3">
-                    logo
+                    <img src="https://imi.id/wp-content/uploads/2023/07/logo-imi.png" alt="Deskripsi Gambar">
                 </div>
                 <div class="col-lg-9">
-                    nama perusahaan
+
+                    <style>
+                        .center-text {
+                            text-align: center;
+                        }
+                    </style>
+                    </head>
+
+                    <body>
+                        <div class="center-text">
+                            <h3>official website of
+                                ikatan motor indonesia</h3>
+                        </div>
+
                 </div>
             </div>
         </div>
