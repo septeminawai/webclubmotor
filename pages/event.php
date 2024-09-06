a3352e3216570d7831c61462c2300dae3aacabc3
Janzen Faidiban <janzenfaidiban@gmail.com>
2024-09-06 21:24:05 +0900

    update events

--- pages/event.php
+++ pages/event.php
@@ -1,5 +1,88 @@
 <div>
-    <h2>Event</h2>
-    <p>Ikatan Motor Indonesia (IMI) selalu memberikan penghargaan dan apresiasi kepada para atlet & tokoh otomotif di Indonesia setiap akhir tahun yang bertujuan agar masyarakat pecinta otomotif semakin termotivasi dalam melakukan kegiatan positif serta bermanfaat dan juga untuk mendorong terciptanya prestasi yang mendunia dalam seluruh aktivitas otomotif.</p>
+    <h2 class="mb-5">Events</h2>
+
+    <!-- .row start -->
+    <div class="row mb-5">
+        <div class="col-lg-4">
+            <img src="./images/produk/balap-motor.jpeg" class="img-fluid img-thumbnail" alt="gambar">
+        </div>
+        <!-- .col end -->
+        <div class="col-lg-8">
+            <h3>Title of the article</h3>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Est ex corrupti dolores sunt nulla voluptatem, totam id, culpa nisi unde ea esse vero quo accusamus! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iste odio assumenda ut accusantium dolores iure aperiam deserunt nam suscipit optio facilis consequatur ex, ad nesciunt voluptatibus quisquam similique, adipisci fugiat.</p>
+            <!-- ul start -->
+            <ul class="list-group">
+                <li class="list-group-item">
+                    <b>Tanggal Mulai:</b> 06/09/2024 - <b>Tanggal Selesai:</b> 07/09/2024
+                </li>
+            </ul>
+            <!-- ul end -->
+        </div>
+        <!-- .col-end -->
+    </div>
+    <!-- .row end -->
+
+    <!-- .row start -->
+    <div class="row mb-5">
+        <div class="col-lg-4">
+            <img src="./images/produk/balap-motor.jpeg" class="img-fluid img-thumbnail" alt="gambar">
+        </div>
+        <!-- .col end -->
+        <div class="col-lg-8">
+            <h3>Title of the article</h3>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Est ex corrupti dolores sunt nulla voluptatem, totam id, culpa nisi unde ea esse vero quo accusamus! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iste odio assumenda ut accusantium dolores iure aperiam deserunt nam suscipit optio facilis consequatur ex, ad nesciunt voluptatibus quisquam similique, adipisci fugiat.</p>
+            <!-- ul start -->
+            <ul class="list-group">
+                <li class="list-group-item">
+                    <b>Tanggal Mulai:</b> 06/09/2024 - <b>Tanggal Selesai:</b> 07/09/2024
+                </li>
+            </ul>
+            <!-- ul end -->
+        </div>
+        <!-- .col-end -->
+    </div>
+    <!-- .row end -->
+
+    <!-- .row start -->
+    <div class="row mb-5">
+        <div class="col-lg-4">
+            <img src="./images/produk/balap-motor.jpeg" class="img-fluid img-thumbnail" alt="gambar">
+        </div>
+        <!-- .col end -->
+        <div class="col-lg-8">
+            <h3>Title of the article</h3>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Est ex corrupti dolores sunt nulla voluptatem, totam id, culpa nisi unde ea esse vero quo accusamus! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iste odio assumenda ut accusantium dolores iure aperiam deserunt nam suscipit optio facilis consequatur ex, ad nesciunt voluptatibus quisquam similique, adipisci fugiat.</p>
+            <!-- ul start -->
+            <ul class="list-group">
+                <li class="list-group-item">
+                    <b>Tanggal Mulai:</b> 06/09/2024 - <b>Tanggal Selesai:</b> 07/09/2024
+                </li>
+            </ul>
+            <!-- ul end -->
+        </div>
+        <!-- .col-end -->
+    </div>
+    <!-- .row end -->
+
+    <!-- .row start -->
+    <div class="row mb-5">
+        <div class="col-lg-4">
+            <img src="./images/produk/balap-motor.jpeg" class="img-fluid img-thumbnail" alt="gambar">
+        </div>
+        <!-- .col end -->
+        <div class="col-lg-8">
+            <h3>Title of the article</h3>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Est ex corrupti dolores sunt nulla voluptatem, totam id, culpa nisi unde ea esse vero quo accusamus! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iste odio assumenda ut accusantium dolores iure aperiam deserunt nam suscipit optio facilis consequatur ex, ad nesciunt voluptatibus quisquam similique, adipisci fugiat.</p>
+            <!-- ul start -->
+            <ul class="list-group">
+                <li class="list-group-item">
+                    <b>Tanggal Mulai:</b> 06/09/2024 - <b>Tanggal Selesai:</b> 07/09/2024
+                </li>
+            </ul>
+            <!-- ul end -->
+        </div>
+        <!-- .col-end -->
+    </div>
+    <!-- .row end -->
 
 </div>
